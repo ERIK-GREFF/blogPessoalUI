fc9ecce4f32c08f91bf5c67aba870e3673995d9c
ERIK-GREFF <83592613+ERIK-GREFF@users.noreply.github.com>
2021-07-07 19:41:11 -0300

    get e post do postagem

--- blogPessoal/src/app/inicio/inicio.component.ts
+++ blogPessoal/src/app/inicio/inicio.component.ts
@@ -1,6 +1,14 @@
+import { getLocaleTimeFormat } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
+import { User } from '../model/User';
+import { AuthService } from '../service/auth.service';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
+import { UsuarioService } from '../service/usuario.service';
 
 @Component({
   selector: 'app-inicio',
@@ -9,8 +17,21 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class InicioComponent implements OnInit {
 
+  postagem: Postagem = new Postagem()
+  listaPostagens: Postagem[]
+
+  tema: Tema = new Tema()
+  idTema:  number
+  listaTemas: Tema[]
+
+  user: User = new User()
+  idUser:number = environment.id
+
   constructor(
-    private router: Router
+    private router: Router,
+    private postagemService: PostagemService,
+    private temaService: TemaService,
+    private usuarioService: UsuarioService
   ) { }
 
   ngOnInit(){
@@ -21,7 +42,51 @@ export class InicioComponent implements OnInit {
       this.router.navigate(['/entrar'])
 
     }
+    this.getAllTemas()
+    console.log()
+    this.getAllPostagens()
     
+  }
+  getAllTemas(){
+    this.temaService.getAllTema().subscribe((resp: Tema[])=>{
+      this.listaTemas = resp
+    })
+  }
+
+  findByIdTema(){
+    this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema)=>{
+      this.tema = resp
+    })
+  }
+
+  getAllPostagens(){
+    this.postagemService.getAllPostagens().subscribe((resp: Postagem[])=>{
+      this.listaPostagens = resp
+    })
+  }
+
+  findByIdUser(){
+    this.usuarioService.getByIdUser(this.idUser).subscribe((resp: User)=>{
+      this.user = resp
+    })
+
+  }
+
+  publicar(){
+    this.tema.id = this.idTema
+    this.postagem.tema = this.tema
+
+    this.user.id = this.idUser
+    this.postagem.usuario = this.user
+    console.log(this.postagem)
+
+    this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem)=>{
+      this.postagem = resp
+      alert('postagem Realizada com sucesso!')
+      this.postagem = new Postagem()
+      this.getAllPostagens()
+    })
   }
 
 }
+
