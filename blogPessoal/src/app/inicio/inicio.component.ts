10ea7ac40c0475c8fd652342e03e74c57e5c6488
ERIK-GREFF <83592613+ERIK-GREFF@users.noreply.github.com>
2021-07-12 11:10:54 -0300

    order by

--- blogPessoal/src/app/inicio/inicio.component.ts
+++ blogPessoal/src/app/inicio/inicio.component.ts
@@ -29,6 +29,10 @@ export class InicioComponent implements OnInit {
   user: User = new User()
   idUser:number = environment.id
 
+
+  key = 'data'
+  reverse = true
+
   constructor(
     private router: Router,
     private postagemService: PostagemService,
